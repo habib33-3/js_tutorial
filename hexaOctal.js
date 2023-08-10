9ceda3cc9fdc566fbf12b7f6ba642bff3af90520
Habibur Rahman <habib.swi16@gmail.com>
2023-08-10 18:04:34 +0600

    typeConversion

---
+++ hexaOctal.js
@@ -0,0 +1,5 @@
+let hex = 0xab;
+console.log(hex);
+
+let octal = 0o757;
+console.log(octal);
---
+++ typeConversion.js
@@ -0,0 +1,7 @@
+let str = "1000";
+let n = 10;
+
+console.log(str * n);
+console.log(str + n);
+console.log(Number.parseInt(str) + n);
+console.log(n.toString());
