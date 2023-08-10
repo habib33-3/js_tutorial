a09b50a600a0965875a077716c070e2260f57e7d
Habibur Rahman <habib.swi16@gmail.com>
2023-08-10 18:42:41 +0600

    today

---
+++ switch.js
@@ -0,0 +1,36 @@
+let date = new Date();
+
+let today = date.getDay();
+
+switch (today) {
+  case 0:
+    console.log("Sunday");
+    break;
+
+  case 1:
+    console.log("Monday");
+    break;
+
+  case 2:
+    console.log("Tuesday");
+    break;
+
+  case 3:
+    console.log("Wednesday");
+    break;
+
+  case 4:
+    console.log("Thursday");
+    break;
+
+  case 5:
+    console.log("Friday");
+    break;
+
+  case 6:
+    console.log("Saturday");
+    break;
+
+  default:
+    break;
+}
