15782e06517c76073f3f7845bf7be76f128321cf
Habibur Rahman <habib.swi16@gmail.com>
2023-08-24 15:07:06 +0600

    composit ion

---
+++ recursion.js
@@ -0,0 +1,36 @@
+function sayHi(n) {
+  if (n === 0) {
+    return;
+  }
+  console.log("hi");
+  sayHi(n - 1);
+}
+
+sayHi(10);
+
+function sum(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n + sum(n - 1);
+}
+console.log(sum(100));
+
+function fact(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * fact(n - 1);
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+function sumOfArray(arr, lastIndex) {
+  if (lastIndex < 0) {
+    return 0;
+  }
+
+  return arr[lastIndex] + sumOfArray(arr, lastIndex - 1);
+}
+
+console.log(sumOfArray(arr, arr.length - 1));
