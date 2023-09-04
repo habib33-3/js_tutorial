e794a5a308410baebb1f0fc7fbfb699ee7e8af54
Habibur Rahman <habib.swi16@gmail.com>
2023-09-05 00:25:49 +0600

    iteratir

---
+++ ch15/iterator.js
@@ -0,0 +1,26 @@
+const arr = [1, 2, 4, 6, 8, 9];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+function createIterator(collection) {
+  let i = 0;
+  return {
+    next() {
+      return {
+        done: i >= collection.length,
+        value: collection[i++],
+      };
+    },
+  };
+}
+
+let iterate = createIterator(arr);
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next());
